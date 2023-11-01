3d019297a14ae844573f86829a07eb3d3a98d33d
marcelkny <schleicher47@googlemail.com>
2023-11-01 15:18:26 +0100

    update

--- cashman-flask-project/cashman/server.py
+++ cashman-flask-project/cashman/server.py
@@ -2,13 +2,15 @@ from flask import Flask, jsonify, request, json
 from ai_chat import ai_chat
 from ai_assist import ai_assist
 from ai_assist_tools import ai_assist_tools
-from config.config import APIKEY, CHAT_MODEL, ASSIST_MODEL
+from config.config import APIKEY, CHAT_MODEL, ASSIST_MODEL, SUM_MODEL
 
 from langchain.llms import GPT4All
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory, ChatMessageHistory
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def isAuthorized(headers):
     auth = headers.get("X-APIKEY")
@@ -16,7 +18,6 @@ def isAuthorized(headers):
         return True
     else:
         return False
-
 
 
 @app.route('/status', methods=['GET'])
@@ -44,6 +45,7 @@ async def ai_chat_func():
 
 @app.route('/chat_reinit_mem', methods=['POST'])
 async def ai_chat_reinit_mem_func():
+    global chatAi
     if isAuthorized(request.headers) == True:
 
         rawData = request.get_json()
@@ -64,6 +66,7 @@ async def ai_chat_reinit_mem_func():
 
 @app.route('/chat_reinit', methods=['POST'])
 async def ai_chat_reinit_func():
+    global chatAi
     if isAuthorized(request.headers) == True:
         chatAi = initializeChatAi()
         response = app.response_class(
@@ -77,84 +80,23 @@ async def ai_chat_reinit_func():
 
 
 
-
-@app.route('/assist', methods=['POST'])
-async def ai_assist_func():
-    if isAuthorized(request.headers) == True:
-
-        print("request: ")
-        print(request.get_json())
-        rawData = request.get_json()
-        aiResponse = await ai_assist(rawData, assistantAi)
-        response = app.response_class(
-            response=aiResponse,
-            status=200,
-            mimetype='application/json'
-        )
-        return response
-    else:
-        return jsonify({"message": "ERROR: Unauthorized"}), 401
-    
-@app.route('/assist_tools', methods=['POST'])
-async def ai_assist_tools_func():
-    if isAuthorized(request.headers) == True:
-
-        print("request: ")
-        print(request.get_json())
-        rawData = request.get_json()
-        aiResponse = await ai_assist_tools(rawData, assistantAiWithTools)
-        response = app.response_class(
-            response=aiResponse,
-            status=200,
-            mimetype='application/json'
-        )
-        return response
-    else:
-        return jsonify({"message": "ERROR: Unauthorized"}), 401
-    
-
-
-def initializeAssistantAi():
-    assist_llm_path = (ASSIST_MODEL)
-    sum_llm_path =  ("cashman/models/Wizard-Vicuna-13B-Uncensored.ggmlv3.q2_K.bin")
-    # Callbacks support token-wise streaming
-    callbacks = [StreamingStdOutCallbackHandler()]
-    chat_llm = GPT4All(model=assist_llm_path, backend="gptj", callbacks=callbacks, verbose=True, max_tokens=2048, temp=1, top_p=0.4, top_k=40, n_batch=512, repeat_penalty=1.2, repeat_last_n=128)
-
-    # Memory for summarizing all in conversation
-    memory = ConversationBufferMemory(memory_key="chat_history_lines", input_key="input", return_messages=True)
-
-    return {"callbacks": callbacks, "chat_llm": chat_llm, "memory": memory}
-
-def initializeAssistantAiWithTools():
-    assist_llm_path = (ASSIST_MODEL)
-    sum_llm_path =  ("cashman/models/Wizard-Vicuna-13B-Uncensored.ggmlv3.q2_K.bin")
-    # Callbacks support token-wise streaming
-    callbacks = [StreamingStdOutCallbackHandler()]
-    chat_llm = GPT4All(model=assist_llm_path, backend="gptj", callbacks=callbacks, verbose=True, max_tokens=2048, temp=1, top_p=0.4, top_k=40, n_batch=512, repeat_penalty=1.2, repeat_last_n=128)
-
-    # Memory for summarizing all in conversation
-    memory = ConversationBufferMemory(memory_key="chat_history")
-
-    return {"callbacks": callbacks, "chat_llm": chat_llm, "memory": memory}
-
 def initializeChatAi():
     print("\n\nstarting to initialize model")
     chat_llm_path = (CHAT_MODEL)
-    sum_llm_path =  ("cashman/models/Wizard-Vicuna-13B-Uncensored.ggmlv3.q2_K.bin")
+    sum_llm_path =  (SUM_MODEL)
     # Callbacks support token-wise streaming
     callbacks = [StreamingStdOutCallbackHandler()]
     chat_llm = GPT4All(model=chat_llm_path, backend="gptj", callbacks=callbacks, verbose=True, max_tokens=2048, temp=1, top_p=0.4, top_k=40, n_batch=512, repeat_penalty=1.2, repeat_last_n=128)
 
     # Memory for summarizing all in conversation
-    memory = ConversationSummaryMemory(llm=chat_llm, input_key="input")
+    memory = ConversationSummaryMemory(llm=chat_llm_path, input_key="input")
 
     return {"callbacks": callbacks, "chat_llm": chat_llm, "memory": memory}
 
 async def initializeChatAiWithExistingMemories(rawConversationData):
     print("\n\nstarting to initialize model with existing memory")
     chat_llm_path = (CHAT_MODEL)
-    sum_llm_path =  ("cashman/models/Wizard-Vicuna-13B-Uncensored.ggmlv3.q2_K.bin")
+    sum_llm_path =  (SUM_MODEL)
     # Callbacks support token-wise streaming
     callbacks = [StreamingStdOutCallbackHandler()]
     chat_llm = GPT4All(model=chat_llm_path, backend="gptj", callbacks=callbacks, verbose=True, max_tokens=2048, temp=1, top_p=0.4, top_k=40, n_batch=512, repeat_penalty=1.2, repeat_last_n=128)
@@ -176,7 +118,7 @@ async def initializeChatAiWithExistingMemories(rawConversationData):
         print("\n\n")
 
     memory = ConversationSummaryMemory.from_messages(
-        llm=chat_llm,
+        llm=chat_llm_path,
         chat_memory=history,
         return_messages=True,
         input_key="input"
@@ -185,5 +127,3 @@ async def initializeChatAiWithExistingMemories(rawConversationData):
     return {"callbacks": callbacks, "chat_llm": chat_llm, "memory": memory}
 
 chatAi = initializeChatAi()
-assistantAi = initializeAssistantAi()
-assistantAiWithTools = initializeAssistantAiWithTools()
